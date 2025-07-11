79c748cbfcb8e573011530d56914373f8909b96e
slingod <slingo.drisca1@gmail.com>
2025-07-11 14:02:51 +0200

    Add historical conflicts, dangerous locations, monuments, and must-visit sites data; update styles with new header color

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,20 +1,12 @@
-// src/components/Globe.jsx
-
 import React from "react";
 import Globe from "react-globe.gl";
 
-const GlobeView = ({ data }) => {
-  // TODO: Convertir les 'data' (tes événements) en points pour le globe
-
+const GlobeView = () => {
   return (
     <Globe
       globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
       backgroundColor="rgba(0,0,0,0)"
-
-      // On préparera ici les données pour les marqueurs du globe
-      // pointsData={...}
-      // pointLat={d => d.lat}
-      // pointLng={d => d.lng}
+      // Prépare les props quand tu en auras besoin
     />
   );
 };
